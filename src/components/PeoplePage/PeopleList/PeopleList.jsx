c7dc7f14566b3c131b37decadb8624b2c86df197
RaEzhov <ezhov.mail@yandex.ru>
2022-10-27 22:49:52 +0300

    Del actions

--- src/components/PeoplePage/PeopleList/PeopleList.jsx
+++ src/components/PeoplePage/PeopleList/PeopleList.jsx
@@ -5,7 +5,7 @@ const PeopleList = ({people}) => {
         <ul className={styles.list__container}>
             {people.map(({id, name, imageUrl}) => {
                 return (<li key={id} className={styles.list__item}>
-                    <a href='#tmp'>
+                    <a href='#'>
                         <img src={imageUrl} alt={name} className={styles.person__photo}/>
                         <p>{name}</p>
                     </a>
